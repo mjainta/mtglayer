97fc0d63652f1334d16d5c875f45f6c124dd4537
Martin Jainta <martin.jainta@tradebyte.com>
2020-08-04 16:29:20 +0200

    Add the launcher.py and crawl.py for programmatic execution

--- mtgcrawler/mtgcrawler/spiders/ckbuylist_spider.py
+++ mtgcrawler/mtgcrawler/spiders/ckbuylist_spider.py
@@ -4,13 +4,24 @@ import scrapy
 class CkbuylistSpider(scrapy.Spider):
     name = "ckbl"
 
-    def start_requests(self):
-        urls = [
-            # 'https://www.cardkingdom.com/purchasing/mtg_singles',
-            'https://www.cardkingdom.com/purchasing/mtg_singles?filter%5Bsort%5D=price_buy_desc&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=&filter%5Bcategory_id%5D=3146&filter%5Bnonfoil%5D=1&filter%5Brarity%5D%5B0%5D=M&filter%5Bprice_op%5D=&filter%5Bprice%5D=&page=1',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://www.cardkingdom.com/purchasing/mtg_singles?filter%5Bsort%5D=price_buy_desc&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=&filter%5Bcategory_id%5D=3146&filter%5Bnonfoil%5D=1&filter%5Brarity%5D%5B0%5D=M&filter%5Bprice_op%5D=&filter%5Bprice%5D=&page=1']
+    allowed_domains = ["cardkingdom.com"]
+    # rules = [  # Get all links on start url
+    #     Rule(
+    #         link_extractor=LinkExtractor(
+    #             deny=r"\?",
+    #         ),
+    #         follow=False,
+    #         callback="parse_page",
+    #     )
+    # ]
+    # def start_requests(self):
+    #     urls = [
+    #         # 'https://www.cardkingdom.com/purchasing/mtg_singles',
+    #         'https://www.cardkingdom.com/purchasing/mtg_singles?filter%5Bsort%5D=price_buy_desc&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=&filter%5Bcategory_id%5D=3146&filter%5Bnonfoil%5D=1&filter%5Brarity%5D%5B0%5D=M&filter%5Bprice_op%5D=&filter%5Bprice%5D=&page=1',
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for quote in response.css('div[class="itemContentWrapper"]'):
