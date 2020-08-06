7187a1bb2df2d5e30f0e94dc8df5697e3b15d0a8
Martin Jainta <martin.jainta@tradebyte.com>
2020-08-06 16:12:30 +0200

    Add terraform files

---
+++ infrastructure/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  profile = var.profile
+  region = "eu-central-1"
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_ec2" {
+  name = "lambda_ec2"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda.json
+}
+
+resource "aws_iam_role_policy" "cloudwatch_ec2" {
+  role = aws_iam_role.lambda_ec2.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  filename = "lambda_function.zip"
+  function_name = "lambda_function"
+  role = aws_iam_role.lambda_ec2.arn
+  handler = "launcher.scrape"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  runtime = "python3.8"
+}
+
+resource "aws_lambda_alias" "lambda_function" {
+  name = "lambda_function"
+  function_name = aws_lambda_function.lambda_function.function_name
+  function_version = "$LATEST"
+}
